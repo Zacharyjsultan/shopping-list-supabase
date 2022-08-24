7d2e54af63c6b4b6578e692a5aa50580aff08046
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-24 11:45:44 -0700

    fetching utils - set up for creating function

--- fetch-utils.js
+++ fetch-utils.js
@@ -43,3 +43,31 @@ export async function signOutUser() {
 }
 
 /* Data functions */
+/// error response 
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
+/// add item from form
+export async function addGrocery(grocery) {
+    return await client.from('groceries').insert(grocery).single();
+    
+}
+
+// get items for list
+export async function getGroceries() {
+    return await client.from('groceries').select('*');
+}
+
+// cross off if completed
+export async function crossOffList(id) {
+    const response = await client.from('groceries').update({ complete: true }).match({ user_id: client.auth.user().id, id:id });
+
+    return checkError(response);
+}
+// delete grocery item
+
+export async function deleteGroceries() {
+    const response = await client.from('groceries').delete().match({ user_id: getUser().id });
+
+    return checkError(response);
+}
